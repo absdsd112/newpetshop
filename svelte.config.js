12ad0f35b5050c1d553ca1642ba83655ab205e9e
absdsd112 <2jonlie@gmail.com>
2025-03-26 21:14:16 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		base: process.env.NODE_ENV === 'production' ? '/newpetshop/' : ''
 	},
 	preprocess: vitePreprocess()
 };
