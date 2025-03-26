850f439ea2c347ff1c2062178268e652fe41964e
absdsd112 <2jonlie@gmail.com>
2025-03-26 20:26:11 +0100

    aa

---
+++ build/_app/immutable/nodes/2.dNVlyweQ.js
@@ -0,0 +1,19 @@
+import{s as P,e as g,a as w,t as F,c as v,b as I,g as O,d as k,f as D,h,i as b,q as T,j as H,k as n,l as S,m as V}from"../chunks/scheduler.DbG560Ij.js";import{S as C,i as E,c as A,a as $,m as W,t as j,b as N,d as q}from"../chunks/index.B_jwHFVU.js";import{P as B}from"../chunks/ProductList.BE6jBJYQ.js";import{t as K}from"../chunks/i18n.CiS_jtXd.js";function R(u){let t,e,p=`<div class="bg-gray-800 bg-opacity-20 p-4 inline-block rounded">Love Starts with Fresh healthy Food
+			<br/> <br/>
+			and Grows with Every Moment Together</div>`,i,o,l,s=u[0]("home.bestseller_title")+"",f,_,c,y,d,M=`<div class="text-3xl font-bold mb-8">The Reasons of Choosing Us</div> <div class="grid grid-cols-1 md:grid-cols-2 gap-8"><div class="text-center"><img src="/meat.png" alt="Meat Icon" class="mx-auto mb-4"/> <div class="font-bold text-2xl">Meat As Numbers #1</div> <br/> <p class="text-lg">The Most Important Thing In A Carnivore’s Life, 
+					<br/>
+					Next To His Family, Is Meat. It’s Exactly 
+					<br/>
+					What He Needs For A Supple Digestive Process.</p></div> <div class="text-center"><img src="/factory.png" alt="Factory Icon" class="mx-auto mb-4"/> <div class="font-bold text-2xl">Local Productions</div> <br/> <p class="text-lg">Our Products Are Locally Produced 
+					<br/>
+					In Our Own Factory In Veldhoven. 
+					<br/>
+					Full Of Virtuous Ingredients. 
+					<br/>
+					That’s Something To Be Truly Proud Of!</p></div> <div class="text-center"><img src="/Happy-dog.png" alt="Dog Icon" class="mx-auto mb-4"/> <div class="font-bold text-2xl">Better Flavour For Pets</div> <br/> <p class="text-lg">Thanks To 50 Years Of Knowledge 
+					<br/>
+					And Craft, We Know Exactly What 
+					<br/>
+					Makes Your Dog Truly Happy.</p></div> <div class="text-center"><img src="/leaf.png" alt="Leaf Icon" class="mx-auto mb-4"/> <div class="font-bold text-2xl">Organic Products</div> <br/> <p class="text-lg">We Make Sure Our Sourcing 
+					<br/>
+				Of Raw Materials Is Organic.</p></div> <br/></div>`,m;return c=new B({props:{products:u[1]}}),{c(){t=g("div"),e=g("div"),e.innerHTML=p,i=w(),o=g("div"),l=g("div"),f=F(s),_=w(),A(c.$$.fragment),y=w(),d=g("div"),d.innerHTML=M,this.h()},l(a){t=v(a,"DIV",{class:!0});var r=I(t);e=v(r,"DIV",{class:!0,style:!0,"data-svelte-h":!0}),O(e)!=="svelte-1vx0wo1"&&(e.innerHTML=p),i=k(r),o=v(r,"DIV",{class:!0});var x=I(o);l=v(x,"DIV",{class:!0});var L=I(l);f=D(L,s),L.forEach(h),_=k(x),$(c.$$.fragment,x),x.forEach(h),y=k(r),d=v(r,"DIV",{class:!0,"data-svelte-h":!0}),O(d)!=="svelte-k9udww"&&(d.innerHTML=M),r.forEach(h),this.h()},h(){b(e,"class","p-80 text-4xl w-screen text-white font-snack tracking-wider bg-blue-400"),T(e,"background-image","url('https://www.pixelstalk.net/wp-content/uploads/wallpapers/dog-and-cat-cocker-spaniel-puppy-small-ginger-kitten-kiss.jpg')"),T(e,"background-size","cover"),T(e,"background-position","center"),b(l,"class","text-2xl"),b(o,"class","pt-8"),b(d,"class","pt-16"),b(t,"class","text-center bg-beige svelte-6hjo9w")},m(a,r){H(a,t,r),n(t,e),n(t,i),n(t,o),n(o,l),n(l,f),n(o,_),W(c,o,null),n(t,y),n(t,d),m=!0},p(a,[r]){(!m||r&1)&&s!==(s=a[0]("home.bestseller_title")+"")&&S(f,s)},i(a){m||(j(c.$$.fragment,a),m=!0)},o(a){N(c.$$.fragment,a),m=!1},d(a){a&&h(t),q(c)}}}function Y(u,t,e){let p;V(u,K,s=>e(0,p=s));let{data:i}=t;const l=i.props.products.filter(s=>s.tags.includes("bestseller"));return u.$$set=s=>{"data"in s&&e(2,i=s.data)},[p,l,i]}class Q extends C{constructor(t){super(),E(this,t,Y,R,P,{data:2})}}export{Q as component};
